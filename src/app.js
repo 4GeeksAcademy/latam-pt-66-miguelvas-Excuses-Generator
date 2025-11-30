2c82827b39a5a9c489c4103cb95dd1ddc4a62d46
miguelvas <44248779+miguelvas@users.noreply.github.com>
2025-11-30 02:45:29 +0000

    Primer commit del generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+    const button = document.getElementById('generate-btn');
 
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    const getRandomExcuse = () => {
+        const randomWho = who[Math.floor(Math.random() * who.length)];
+        const randomAction = action[Math.floor(Math.random() * action.length)];
+        const randomWhat = what[Math.floor(Math.random() * what.length)];
+        const randomWhen = when[Math.floor(Math.random() * when.length)];
+        
+        return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+    };
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    button.addEventListener('click', () => {
+        const excuse = getRandomExcuse();
+        const display = document.getElementById('excuse');
+        display.textContent = excuse;
+    });
+
+    console.log(getRandomExcuse());
+    
+    const excuses = [];
 };
