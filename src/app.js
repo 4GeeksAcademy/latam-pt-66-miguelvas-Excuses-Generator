cb8e8246b19952e06468a982c9a241fd9c5df92b
miguelvas <44248779+miguelvas@users.noreply.github.com>
2026-01-27 03:07:21 +0000

    Refactorizacion

--- src/app.js
+++ src/app.js
@@ -1,27 +1,20 @@
-const button = document.getElementById('generate-btn');
-
-    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-    let action = ['ate', 'peed', 'crushed', 'broke'];
-    let what = ['my homework', 'my phone', 'the car'];
-    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-    
-    const getRandomExcuse = () => {
-        const randomWho = who[Math.floor(Math.random() * who.length)];
-        const randomAction = action[Math.floor(Math.random() * action.length)];
-        const randomWhat = what[Math.floor(Math.random() * what.length)];
-        const randomWhen = when[Math.floor(Math.random() * when.length)];
-        
-        return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
-    };
-
 window.onload = () => {
+    const button = document.getElementById('generate-btn');
+    const excuse = document.getElementById('excuse');
+    
+    const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    const action = ['ate', 'peed', 'crushed', 'broke'];
+    const what = ['my homework', 'my phone', 'the car'];
+    const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch','while I was praying'];
+    
+    const random = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-    let excuseDefault = document.querySelector('#excuse');
-    excuseDefault.innerText =getRandomExcuse();
+    const getRandomExcuse = () =>
+        `${random(who)} ${random(action)} ${random(what)} ${random(when)}.`;
+
+    excuse.textContent = getRandomExcuse();
 
     button.addEventListener('click', () => {
-        const excuse = getRandomExcuse();
-        const display = document.getElementById('excuse');
-        display.textContent = excuse;
+        excuse.textContent = getRandomExcuse();
     });
 };
